3d7f546bbc2da3ffd36d0c1e16609c1c8959082d
amanguptacloud <amanguptashiina@gmail.com>
2026-07-19 00:10:53 +0530

    added 2nd method in ques 12

--- Q-12 p2.c
+++ Q-12 p2.c
@@ -1,5 +1,6 @@
 // Swap two numbers using a third variable and again without using a third variable.
-
+// methods without a third variable 
+// 1st methode
 #include <stdio.h>
 
 int main() {
@@ -11,6 +12,18 @@ int main() {
     b = a-b;
     a = a-b;
 
-    printf("a = %d,b = %d",a,b);
+    printf("a = %d,b = %d\n",a,b);
+
+    // 2nd method 
+    int f,s;
+    printf("enter numbers: ");
+    scanf("%d%d",&f,&s);
+
+    f= f^s;
+    s= f^s;
+    f= f^s;
+
+    printf("a= %d, b= %d\n",f,s);
+
     return 0;
 }
